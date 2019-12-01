80858ac4cd603712e03e4c13c8306605b1f42a31
Jairo Luiz <t4ngz3r0@gmail.com>
2019-12-01 15:47:07 -0300

    added all the information needed to solve the day 01

--- day01/src/main/kotlin/dev/tangzero/aoc2019/App.kt
+++ day01/src/main/kotlin/dev/tangzero/aoc2019/App.kt
@@ -1,12 +1,5 @@
 package dev.tangzero.aoc2019
 
-class App {
-    val greeting: String
-        get() {
-            return "Hello world."
-        }
-}
-
-fun main(args: Array<String>) {
-    println(App().greeting)
+fun main() {
+    println("Day 01")
 }
